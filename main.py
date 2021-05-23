54cca21c507a37941d1050ea88ac140c6606bcad
Nick Watson <>
2021-05-23 13:58:15 +0000

    Added problem #3

--- main.py
+++ main.py
@@ -54,3 +54,39 @@ create var nums_2; iterate through nums, if integer is unique, add to nums_2, if
 '''
 
 # ===============
+
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        
+        counts = {}
+        
+        for i in arr:
+            if i not in counts:
+                counts[i] = arr.count(i)
+                
+        if len(counts) != len(set(counts.values())):
+            return False
+            
+        return True
+        
+        
+'''
+
+U:
+
+[1, 2, 2, 1, 1, 3]
+output = True
+
+[1, 2, 3, 4, 5]
+output = False
+
+[1, 1, 2, 3, 3, 3, 4]
+output = False
+
+P:
+
+Create Dict count; iterate through arr.  if i is not in count, add to count, with count()  set as value.  If all values in count are unique, return True, otherwise, return False
+
+'''
+
+# ===============
