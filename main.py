ff8cb13c47ef4008ea6f4235de9b91d9747257fe
Nick Watson <>
2021-05-23 13:39:03 +0000

    Added problem #2

--- main.py
+++ main.py
@@ -29,3 +29,28 @@ iterate through stones; if i is in jewels, increase var count by 1
 
 # ===============
 
+class Solution:
+    def sumOfUnique(self, nums: List[int]) -> int:
+        
+        nums_2 = 0
+        
+        for num in nums:
+            if nums.count(num) == 1:
+                nums_2 += num
+                
+        return nums_2
+        
+'''
+
+U:
+
+[1, 2, 3, 4, 5, 3, 2]
+output = 10
+
+P:
+
+create var nums_2; iterate through nums, if integer is unique, add to nums_2, if not; skip
+
+'''
+
+# ===============
